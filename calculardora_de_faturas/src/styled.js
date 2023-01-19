fdbeefe5424153da2957261ae29409cc0c277a1d
Caio S.G <caiosilvestre.dev@gmail.com>
2023-01-19 13:01:20 -0300

    Criação do Component Resultados, alteacoes nos arquivos nativos do React

---
+++ calculardora_de_faturas/src/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Main = styled.div`
+display: flex;
+flex-direction:row;
+align-items: center;
+justify-content: center;
+width: fit-content;
+border-radius: 10px 10px 10px 10px;
+box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.60);
+
+`
